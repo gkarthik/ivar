0917ff58832f0e1ed9d27667cded6321e8c32127
gkarthik <xraydiffraction@gmail.com>
2018-05-22 21:50:44 -0700

    Rudimentary additions to deal with indels.

--- call_consensus.cpp
+++ call_consensus.cpp
@@ -16,30 +16,34 @@ struct ad {
 };
 
 int get_majority(bcf_fmt_t fmt, ad val){
-  int maj_ind;
+  int maj_ind = -1;
+  val.maj_v8 = 1;
+  val.maj_v16 = 1;
+  val.maj_v32 = 1;
+  val.maj_vf = 1;
   for (int j = 0; j < fmt.n; ++j){
     // std::cout << p[j] << ":";
     switch(val.type){
     case 1:
-      if(val.v8[j] > val.maj_v8){
+      if(val.v8[j] >= val.maj_v8){
 	maj_ind = j;
 	val.maj_v8 = val.v8[j];
       }
       break;
     case 2:
-      if(val.v16[j] > val.maj_v16){
+      if(val.v16[j] >= val.maj_v16){
 	maj_ind = j;
 	val.maj_v16 = val.v16[j];
       }
       break;
     case 3:
-      if(val.v32[j] > val.maj_v32){
+      if(val.v32[j] >= val.maj_v32){
 	maj_ind = j;
 	val.maj_v32 = val.v32[j];
       }
       break;
     case 4:
-      if(val.vf[j] > val.maj_vf){
+      if(val.vf[j] >= val.maj_vf){
 	maj_ind = j;
 	val.maj_vf = val.vf[j];
       }
@@ -82,44 +86,59 @@ int main(int argc, char* argv[]) {
     bcf1_t *v = bcf_init();
     int ctr = 0;
     int prev_pos = 0;
+    int s;
     while(bcf_read(in, header, v) ==0){
       // std::cout << "Position: " << v->pos << std::endl;
       // std::cout << "Number of Alleles: " << v->n_allele << std::endl;
       for (int i = prev_pos; i < v->pos - 1; ++i){
 	out << "?";
       }
-      if(v->n_allele == 1){
-	bcf_unpack(v, BCF_UN_STR);
-	out << v->d.allele[0];
-      } else {
-	bcf_unpack(v, BCF_UN_FMT);
-	bcf_fmt_t *fmt = v->d.fmt;
-	for (int i = 0; i < v->n_fmt; ++i){
-	  // std::cout << "ID: " << fmt[i].id << std::endl;
-	  // std::cout << " Type: " << fmt[i].type << std::endl;
-	  // std::cout <<" Size: " << fmt[i].size << std::endl;
-	  // std::cout << "String: " << header->id[BCF_DT_ID][fmt[i].id].key << std::endl;
-	  if(strcmp(header->id[BCF_DT_ID][fmt[i].id].key, "AD") == 0){
-	    int maj_ind = -1;
-	    ad tmp;
-	    tmp.type = fmt[i].type;
-	    switch(fmt[i].type){
-	    case BCF_BT_INT8: tmp.v8 = (int8_t*) fmt[i].p; break;
-	    case BCF_BT_INT16: tmp.v16 = (int16_t*) fmt[i].p; break;
-	    case BCF_BT_INT32: tmp.v32 = (int32_t*) fmt[i].p; break;
-	    case BCF_BT_FLOAT: tmp.vf = (float*) fmt[i].p; break;
-	    default: hts_log_error("Unexpected type %d", fmt[i].type); exit(1);
-	    }
-	    maj_ind = get_majority(fmt[i], tmp);
-	    if(v->pos == 1903){
-	      std::cout << v->d.allele[maj_ind] << " " << maj_ind << std::endl;
-	    }
-	    // std::cout << "Indice: " << maj_ind << std::endl;
-	    bcf_unpack(v, BCF_UN_STR);
-	    // std::cout << v->d.als[maj_ind] << std::endl;
-	    out << v->d.allele[maj_ind];
-	    // std::cout << std::endl << std::endl;
+      bcf_unpack(v, BCF_UN_FMT);
+      bcf_fmt_t *fmt = v->d.fmt;
+      for (int i = 0; i < v->n_fmt; ++i){
+	// std::cout << "ID: " << fmt[i].id << std::endl;
+	// std::cout << " Type: " << fmt[i].type << std::endl;
+	// std::cout <<" Size: " << fmt[i].size << std::endl;
+	// std::cout << "String: " << header->id[BCF_DT_ID][fmt[i].id].key << std::endl;
+	if(strcmp(header->id[BCF_DT_ID][fmt[i].id].key, "AD") == 0){
+	  int maj_ind = -1;
+	  ad tmp;
+	  tmp.type = fmt[i].type;
+	  switch(fmt[i].type){
+	  case BCF_BT_INT8: tmp.v8 = (int8_t*) fmt[i].p; break;
+	  case BCF_BT_INT16: tmp.v16 = (int16_t*) fmt[i].p; break;
+	  case BCF_BT_INT32: tmp.v32 = (int32_t*) fmt[i].p; break;
+	  case BCF_BT_FLOAT: tmp.vf = (float*) fmt[i].p; break;
+	  default: hts_log_error("Unexpected type %d", fmt[i].type); exit(1);
 	  }
+	  maj_ind = get_majority(fmt[i], tmp);
+	  if(maj_ind == -1){
+	    out << "?";
+	  } else {
+	    bcf_unpack(v, BCF_UN_STR);
+	    int d = strlen(v->d.allele[0]) - strlen(v->d.allele[maj_ind]); // Ref - Allele
+	    std::cout << "Length " << d << std::endl;
+	    if(d == 0){
+	      if(strlen(v->d.allele[maj_ind]) == 1){
+		out << v->d.allele[maj_ind];
+	      }		       // Else there is no indel as it matches reference
+	    } else if(d > 0) {	// Deletion
+	      s = strlen(v->d.allele[0]);
+	      for (int j = d; j >0; --j){
+		out << v->d.allele[0][s-d];
+	      }
+	    } else if(d < 0) {	// Insertion
+	      s = strlen(v->d.allele[maj_ind]);
+	      d = -1 * d;
+	      for (int j = d; j > 0; --j){
+		out << v->d.allele[maj_ind][s-d];
+	      }
+	    }
+	  }
+	  // std::cout << "Indice: " << maj_ind << std::endl;
+	  // std::cout << v->d.als[maj_ind] << std::endl;
+
+	  // std::cout << std::endl << std::endl;
 	}
       }
       prev_pos = v->pos;
