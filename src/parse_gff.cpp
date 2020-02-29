446b1f96e0a7482db45ee6952864933fb3f5b600
gkarthik <xraydiffraction@gmail.com>
2020-02-28 17:32:35 -0800

    Added GFF3 file format in comment.

--- src/parse_gff.cpp
+++ src/parse_gff.cpp
@@ -2,6 +2,20 @@
 #include <fstream>
 #include <sstream>
 
+/* 
+GFF3 file format:
+| Col        | Desc                                                          |
+| seqid      | ID of ref                                                     |
+| source     | algorithm/operating procedure                                 |
+| type       | type of feature                                               |
+| start      | 1-based start                                                 |
+| end        | start <= end                                                  |
+| score      | score of feature                                              |
+| strand     | +/-                                                           |
+| phase      | Number of bases to be removed with reference to reading frame |
+| attributes | list of attributes in the format tag=value;tag-value..        |
+*/
+
 int read_gff(std::string file_path){
   std::string line, cell;
   std::stringstream line_stream;
@@ -13,22 +27,24 @@ int read_gff(std::string file_path){
     while(std::getline(line_stream,cell,'\t')){
       switch(ctr){
       case 0:
-
 	break;
       case 1:
-
 	break;
       case 2:
-
 	break;
       case 3:
-
 	break;
       case 4:
 	break;
       case 5:
 	break;
       case 6:
+	break;
+      case 7:
+	break;
+      case 8:
+	break;
+      default:
 	break;
       }
       ctr++;
