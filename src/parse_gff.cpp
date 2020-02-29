be2f2af2f4118f6e8636351a50fc1b986cab4329
gkarthik <xraydiffraction@gmail.com>
2020-02-28 22:11:19 -0800

    Added functions to read GFF3 file.

--- src/parse_gff.cpp
+++ src/parse_gff.cpp
@@ -1,55 +1,118 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <regex>
+#include <map>
+#include <iterator>
 
-/* 
-GFF3 file format:
-| Col        | Desc                                                          |
-| seqid      | ID of ref                                                     |
-| source     | algorithm/operating procedure                                 |
-| type       | type of feature                                               |
-| start      | 1-based start                                                 |
-| end        | start <= end                                                  |
-| score      | score of feature                                              |
-| strand     | +/-                                                           |
-| phase      | Number of bases to be removed with reference to reading frame |
-| attributes | list of attributes in the format tag=value;tag-value..        |
-*/
+#include "parse_gff.h"
 
-int read_gff(std::string file_path){
-  std::string line, cell;
-  std::stringstream line_stream;
-  std::ifstream fin = std::ifstream(file_path);
+gff3_feature::gff3_feature(std::string line){
   int ctr = 0;
-  while (std::getline(fin, line)){
-    line_stream << line;
-    ctr = 0;
-    while(std::getline(line_stream,cell,'\t')){
-      switch(ctr){
-      case 0:
-	break;
-      case 1:
-	break;
-      case 2:
-	break;
-      case 3:
-	break;
-      case 4:
-	break;
-      case 5:
-	break;
-      case 6:
-	break;
-      case 7:
-	break;
-      case 8:
-	break;
-      default:
-	break;
-      }
-      ctr++;
+  std::stringstream line_stream;
+  std::string cell;
+  line_stream << line;
+  while(std::getline(line_stream,cell,'\t')){
+    switch(ctr){
+    case 0:			// Name
+      this->seqid = cell;
+      break;
+    case 1:
+      this->source = cell;
+      break;
+    case 2:
+      this->type = cell;
+      break;
+    case 3:
+      this->start = atoi(cell.c_str());
+      break;
+    case 4:
+      this->end = atoi(cell.c_str());
+      break;
+    case 5:
+      this->score = atof(cell.c_str());
+      break;
+    case 6:
+      this->strand = cell[0];
+      break;
+    case 7:
+      this->phase = cell[0];
+      break;
+    case 8:
+      this->set_attributes(cell);
+      break;
     }
-    line_stream.clear();
+    ctr++;
   }
+  line_stream.clear();
+}
+
+int gff3_feature::print(){
+  std::cout << seqid << "\t"
+	    << source << "\t"
+    	    << type << "\t"
+    	    << start << "\t"
+	    << end << "\t"
+	    << score << "\t"
+	    << strand << "\t"
+	    << phase << "\t";
+  std::map<std::string, std::string>::iterator it;
+  for (it = attributes.begin(); it != attributes.end(); it++){
+    std::cout << it->first << ": " << it->second << "; ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
+
+std::string gff3_feature::get_attr(std::string key){
+  std::string val;
+  if(attributes.find(key) != attributes.end()){
+    val = attributes[key];
+  }
+  return val;
+}
+
+int gff3_feature::set_attributes(std::string attr){
+  std::string key, val;
+  std::regex exp("[^;]+");
+  std::regex_iterator<std::string::iterator> it(attr.begin(), attr.end(), exp);
+  std::regex_iterator<std::string::iterator> rend;
+  std::string delimiter = "=";
+  while (it!=rend) {
+    key = it->str().substr(0, it->str().find(delimiter));
+    val = it->str().substr(it->str().find(delimiter)+1, it->str().length());
+    if(!key.empty() && !val.empty()){
+      this->attributes[key] = val;
+    }
+    ++it;
+  }
+  return 0;
+}
+
+int gff3::print(){
+  std::vector<gff3_feature>::iterator it;
+  for(it = features.begin(); it != features.end(); it++){
+    it->print();
+  }
+  return 0;
+}
+
+std::vector<gff3_feature> gff3::get_features(){
+  return features;
+}
+
+gff3::gff3(std::ifstream &fin){
+  std::string line;
+  while (std::getline(fin, line)){
+    if(line[0] == '#' && line[1] == '#') // Avoid comments in GFF file
+      continue;
+    features.push_back(gff3_feature(line));
+  }
+}
+
+int main(int argc, char *argv[]){
+  std::ifstream fin = std::ifstream(argv[1]);
+  gff3 gff(fin);
+  gff.print();
   return 0;
 }
---
+++ src/parse_gff.h
@@ -0,0 +1,64 @@
+#include<string>
+#include<map>
+#include <vector>
+
+#ifndef parse_gff
+#define pare_gff
+
+/* 
+GFF3 file format:
+| Col        | Desc                                                          |
+| seqid      | ID of ref                                                     |
+| source     | algorithm/operating procedure                                 |
+| type       | type of feature                                               |
+| start      | 1-based start                                                 |
+| end        | start <= end                                                  |
+| score      | score of feature                                              |
+| strand     | +/-                                                           |
+| phase      | Number of bases to be removed with reference to reading frame |
+| attributes | list of attributes in the format tag=value;tag-value..        |
+*/
+
+class gff3_feature{
+public:
+  gff3_feature(std::string line);
+  int print();
+  
+  std::string get_seqid();
+  std::string get_source();
+  std::string get_type();
+  uint64_t get_start();
+  uint64_t get_end();
+  char get_strand();
+  char get_phase();
+  std::map<std::string, std::string> get_attributes();
+  std::string get_attr(std::string key);
+
+  int set_seqid();
+  int set_source();
+  int set_type();
+  int set_start();
+  int set_end();
+  int set_strand();
+  int set_phase();
+  int set_attributes(std::string attr);
+private:
+  std::string seqid, source, type;
+  std::map<std::string, std::string> attributes;
+  uint64_t start, end;
+  float score;
+  char strand, phase;
+};
+
+
+class gff3{
+public:
+  gff3(std::ifstream &fin);
+  std::vector<gff3_feature> get_features();
+  int print();
+
+private:
+  std::vector<gff3_feature> features;
+};
+
+#endif
