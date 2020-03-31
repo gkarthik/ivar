ab69a8f83d1aca1d76ba508ab9856e3bca665642
gkarthik <xraydiffraction@gmail.com>
2020-03-30 21:53:57 -0700

    Added test for seg fault if entire column is deletion

--- tests/test_call_consensus_from_plup.cpp
+++ tests/test_call_consensus_from_plup.cpp
@@ -3,7 +3,7 @@
 #include "../src/allele_functions.h"
 
 int main() {
-  int num_tests = 6;
+  int num_tests = 8;
   allele a1 = {
     "A",
     40,
@@ -55,18 +55,46 @@ int main() {
     ad.at(i) = arr[i];
   }
   s = get_consensus_allele(ad, 0, 0, 'N');
-  std::cout << s.nuc << ": " << s.q << std::endl;
   success += (s.nuc.compare("DW") == 0) ? 1: 0;
   success += (s.q.compare("8+") == 0) ? 1 : 0;
   ad.push_back(a6);
   s = get_consensus_allele(ad, 0, 0, 'N');
-  std::cout << s.nuc << ": " << s.q << std::endl;
   success += (s.nuc.compare("DAS") == 0) ? 1: 0;
   success += (s.q.compare("8++") == 0) ? 1 : 0;
   ad.push_back(a7);
   s = get_consensus_allele(ad, 0, 0, 'N');
-  std::cout << s.nuc << ": " << s.q << std::endl;
   success += (s.nuc.compare("DAB") == 0) ? 1: 0;
   success += (s.q.compare("8++") == 0) ? 1 : 0;
+  allele a8 = {
+    "-AT",
+    10,
+    5,
+    20
+  };
+  allele a9 = {
+    "-A",
+    10,
+    5,
+    20
+  };
+  allele a10 = {
+    "-AG",
+    10,
+    5,
+    20
+  };
+  allele a11 = {
+    "-AGT",
+    10,
+    5,
+    20
+  };
+
+  allele del_arr[] = {a8, a9, a10, a11};
+  ret_t del_s;
+  std::vector<allele> del_ad(del_arr, del_arr+sizeof(del_arr)/sizeof(allele));
+  s = get_consensus_allele(del_ad, 0, 0, 'N');
+  success += (s.nuc.compare("") == 0) ? 1: 0;
+  success += (s.q.compare("") == 0) ? 1 : 0;
   return (success == num_tests) ? 0 : -1;
 }
