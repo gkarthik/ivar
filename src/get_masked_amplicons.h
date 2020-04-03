224e27fbb1f6e8d916bb594bbb5c3b34393b6284
gkarthik <xraydiffraction@gmail.com>
2020-04-03 14:43:57 -0700

    Changed from indice to looking for primer directly

--- src/get_masked_amplicons.h
+++ src/get_masked_amplicons.h
@@ -8,7 +8,6 @@
 #ifndef get_masked_amplicons
 #define get_masked_amplicons
 
-int get_primer_indice(std::vector<primer> p, unsigned int pos);
 int get_primers_with_mismatches(std::string bed, std::string vpath, std::string out, std::string primer_pair_file);
 
 #endif
