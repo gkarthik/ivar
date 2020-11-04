3db32a4c690b79bdf61e90b3035943e4dc3058ee
Alaa Abdel Latif <al.a.latif.94@gmail.com>
2020-11-04 10:08:43 -0800

    unit test for amplicon filter step added to ivar trim (using -f option)

--- tests/test_amplicon_search.cpp
+++ tests/test_amplicon_search.cpp
@@ -1,60 +1,47 @@
 #include <iostream>
 #include <vector>
 
+#include "../src/trim_primer_quality.h"
 #include "../src/primer_bed.h"
 #include "../src/interval_tree.h"
 
-IntervalTree test_populate_amplicons(std::string pair_info_file, std::string bed_file)
+int test_amplicon_search(std::string bam_file, std::string bed_file, std::string pair_info_file, bool expected[])
 {
     std::vector<primer> primers;
+    IntervalTree amplicons;
     primers = populate_from_file(bed_file);
-    int amplicon_start = -1;
-    int amplicon_end = -1;
-    IntervalTree tree = IntervalTree();
     std::cout << "Total Number of primers: " << primers.size() << std::endl;
-    populate_pair_indices(primers, pair_info_file);
-    for (auto & p : primers) {
-        if (p.get_strand() == '+')
-        {
-            if (p.get_pair_indice() != -1){
-                amplicon_start = p.get_start();
-                amplicon_end = primers[p.get_pair_indice()].get_end();
-                std::cout << amplicon_start << ":" << amplicon_end << std::endl;
-                tree.insert(Interval(amplicon_start, amplicon_end));
-
-            }
+    amplicons = populate_amplicons(pair_info_file, primers);
+    samFile *in = hts_open(bam_file.c_str(), "r");
+    sam_hdr_t *hdr = sam_hdr_read(in);
+    bam1_t *aln = bam_init1();
+    int cnt = 0;
+    int res = 0;
+    while(sam_read1(in, hdr, aln) >= 0) {
+        res = amplicon_filter(amplicons, aln);
+        if(res != expected[cnt]){
+           	std::cout << "Amplicon filter output did not match " << bam_get_qname(aln)  << " Expected " <<  expected[cnt]  << " " 
+               << "Got " << res << std::endl;
+            std::cout << "Read intervals: " << aln->core.pos << ":" << bam_endpos(aln) << std::endl;
+            return -1;
         }
-    }
-    return tree;
-}
-
-int test_itree_overlap(IntervalTree tree, Interval queries[], int num_tests, bool expected[]){
-    int result = 0;
-    for (int i = 0; i < num_tests; i++)
-    {
-        result = tree.overlapSearch(queries[i]);
-        if (result != expected[i])
-        {
-            std::cout << "Interval Tree overlap behavior incorrect for interval " << queries[i].low << ":" << queries[i].high 
-            << " - " << "Expected: " << expected[i] << " Got: " << result << std::endl;
-            return 1;
-        }
+        cnt++;
     }
     return 0;
 }
 
 
 int main(){
-    int result = 0;
-    std::string pair_indice = "../data/pair_information.tsv";
-    std::string bed = "../data/test.bed";
-    IntervalTree tree;
+    int success;
+    std::string bam = "../data/test_amplicon.sorted.bam";
+    std::string pair_indice = "../data/pair_info_2.tsv";
+    std::string bed = "../data/test_isize.bed";
+    std::string prefix = "/tmp/data/trim_amplicon";
+    std::string bam_out = "/tmp/trim_amplicon.bam";
 
-    tree = test_populate_amplicons(pair_indice, bed);
-    tree.inOrder();
-    Interval queries[6] = {Interval(8, 380), Interval(230, 679), Interval(10, 250), Interval(10, 390), Interval(220, 600), Interval(240, 680)};
-    bool expected[6] = {true, true, true, false, false, false};
-    int num_tests = sizeof(queries) / sizeof(queries[0]);
-    result = test_itree_overlap(tree, queries, num_tests, expected);
-    return result;
+    IntervalTree amplicons;
+    bool expected[8] = {true, false, true, false, true, false, true, false};
+    success = test_amplicon_search(bam, bed, pair_indice, expected);
+    amplicons.inOrder();    
+    return success;
 }
